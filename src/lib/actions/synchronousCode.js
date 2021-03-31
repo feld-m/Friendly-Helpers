ef91d3780ac611127cea6bd200399738186fdc04
Ariak <ariakdev@gmail.com>
2021-03-31 13:53:49 +0200

    sequencing support

--- src/lib/actions/synchronousCode.js
+++ src/lib/actions/synchronousCode.js
@@ -2,8 +2,9 @@
 
 module.exports = function (settings, event) {
   try {
-    settings.callback.apply(event.element || this, [event, event.target]);
-    return true;
+    // return callback result (if any) in case a promise is returned by user defined custom code
+    // see: https://experienceleaguecommunities.adobe.com/t5/adobe-experience-platform-launch/rule-component-sequencing/ba-p/362099
+    return settings.callback.apply(event.element || this, [event, event.target]); 
   } catch (e) {
     turbine.logger.error('Error running synchronous code event', settings, event, '\n', e);
     return false;
