c17967c6aab86b2e731d3fb0264cb3156b6a4b3f
Ariak <ariakdev@gmail.com>
2021-03-31 13:26:48 +0200

    custom code true/false

--- src/lib/actions/synchronousCode.js
+++ src/lib/actions/synchronousCode.js
@@ -3,8 +3,9 @@
 module.exports = function (settings, event) {
   try {
     settings.callback.apply(event.element || this, [event, event.target]);
+    return true;
   } catch (e) {
     turbine.logger.error('Error running synchronous code event', settings, event, '\n', e);
+    return false;
   }
-  return true;
 };
