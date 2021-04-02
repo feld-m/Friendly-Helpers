e10e43ab9c2b4b8f14773bb22cf5d2ed447aa587
Ariak <ariakdev@gmail.com>
2021-04-02 11:21:57 +0200

    add promise to syncronous code

--- src/lib/actions/synchronousCode.js
+++ src/lib/actions/synchronousCode.js
@@ -1,12 +1,15 @@
 'use strict';
 
+// for backward compatability
+var Promise = require('@adobe/reactor-promise');
+
 module.exports = function (settings, event) {
   try {
     // return callback result (if any) in case a promise is returned by user defined custom code
     // see: https://experienceleaguecommunities.adobe.com/t5/adobe-experience-platform-launch/rule-component-sequencing/ba-p/362099
-    return settings.callback.apply(event.element || this, [event, event.target]); 
+    return settings.callback.call(event.element, event, event.target, Promise);
   } catch (e) {
-    turbine.logger.error('Error running synchronous code event', settings, event, '\n', e);
+    turbine.logger.error('Error running Synchronous Code action\n', 'Triggering event:', event, '\n', e);
     return false;
   }
 };
