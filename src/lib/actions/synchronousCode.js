055d0edf9934b09441aadc144ad0b796e7340a55
Ariak <ariakdev@gmail.com>
2021-04-02 11:38:57 +0200

    better debugging

--- src/lib/actions/synchronousCode.js
+++ src/lib/actions/synchronousCode.js
@@ -1,6 +1,5 @@
 'use strict';
 
-// for backward compatability
 var Promise = require('@adobe/reactor-promise');
 
 module.exports = function (settings, event) {
@@ -9,7 +8,7 @@ module.exports = function (settings, event) {
     // see: https://experienceleaguecommunities.adobe.com/t5/adobe-experience-platform-launch/rule-component-sequencing/ba-p/362099
     return settings.callback.call(event.element, event, event.target, Promise);
   } catch (e) {
-    turbine.logger.error('Error running Synchronous Code action\n', 'Triggering event:', event, '\n', e);
+    turbine.logger.error('Failed to run "Synchronous Code" for "' + event.$rule + '" rule.\nEvent:', event, '\nError:', e);
     return false;
   }
 };
